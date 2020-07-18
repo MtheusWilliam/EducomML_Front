419009ec088fbec2b737859141a808a3d188b0a9
Matheus William <matheuswg99@gmail.com>
2020-07-17 23:39:58 -0300

    Novo layout página inicial

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,43 +1,161 @@
 <template>
   <v-row class="text-center">
-    <v-img src="@/assets/home.jpg" height="40vh">
-      <v-toolbar class="black"  style="opacity:0.6;">
+    <v-img :src="atual_page.img_src" height="40vh">
+      <v-toolbar class="pt-1" style="box-shadow:none; background-color: rgba(0, 0, 0, .6);">
+        <v-toolbar-title dark class="ml-5 pt-1">
+          <v-img src="@/assets/logo.png" style="width: 13vw;"></v-img>
+        </v-toolbar-title>
 
-      <v-toolbar-title dark class="ml-5"><v-img src="@/assets/logoblack.png" style="width: 8vw;"></v-img></v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn  dark class="black" style="opacity:0.6;">
-        Pagina Inicial
-      </v-btn>
-
-      <v-btn  dark class="black" style="opacity:0.6;">
-        Equipe
-      </v-btn>
-
-      <v-btn  dark class="black" style="opacity:0.6;">
-        Projeto
-      </v-btn>
-      <v-btn  icon dark class="black" style="opacity:0.6;">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-toolbar>
-    
+        <v-spacer></v-spacer>
+        <v-tabs
+          class="d-flex justify-end mr-1"
+          v-model="tab"
+          background-color="transparent"
+          active-class="white--text"
+          hide-slider
+          show-arrows="false"
+        >
+          <v-tab
+            @click="atual_page = pages[0]"
+            style="background-color: rgba(0, 0, 0, .01); box-shadow:none; color:white;"
+          >Pagina Inicial</v-tab>
+          <v-tab
+            @click="atual_page = pages[1]"
+            style="background-color: rgba(0, 0, 0, .01); box-shadow:none; color:white;"
+          >Projeto</v-tab>
+          <v-tab
+            @click="atual_page = pages[2]"
+            style="background-color: rgba(0, 0, 0, .01); box-shadow:none; color:white;"
+          >Equipe</v-tab>
+        </v-tabs>
+      </v-toolbar>
       <v-row style="height:50%;opacity:0.6;" align="end" class="black" justify="center">
-        <h1 style="color:white; font-size: 4.4em;">Educom Tool!</h1>
+        <h1 style="color:white; font-size: 4.4em;">{{atual_page.title}}</h1>
       </v-row>
-      <v-row style="height:50%;opacity:0.6;font-size: 1.4em" align="start" class="black white--text" justify="center">
-        Educational content modelling
-      </v-row>
+      <v-row
+        style="height:50%;opacity:0.6;font-size: 1.4em"
+        align="start"
+        class="black white--text"
+        justify="center"
+      >{{atual_page.subtitle}}</v-row>
     </v-img>
-    <v-row align="start" class="grey white--text" style="padding:4vh;width:100vw;" justify="center">
-        <h4 >Educom é uma abordagem de modelagem que auxilia<br>
-         professores e autores a modelar conteúdo didático de domínio<br> do conhecimento. </h4>
-      </v-row>
-    <v-row align="start" justify="center">
-      <v-btn outlined color="primary" class="ma-8" width="15vw" dark>Acesse aqui</v-btn>
-    </v-row>
-      
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-row
+          align="start"
+          class="black--text"
+          style="background-color: #E5E7E9 ;padding:4vh;width:101vw;"
+          justify="center"
+        >
+          <h3>
+            Educom é uma abordagem de modelagem que auxilia
+            <br />professores e autores a modelar conteúdo didático de
+            <br />domínio do conhecimento.
+          </h3>
+        </v-row>
+        <v-row align="start" justify="center">
+          <v-btn outlined color="#B19114" class="ma-8" width="20vw" dark>Acesse aqui</v-btn>
+        </v-row>
+      </v-tab-item>
+      <v-tab-item></v-tab-item>
+      <v-tab-item>
+        <v-row
+          align="start"
+          class="black--text"
+          style="background-color: white ;padding:4vh;width:101vw;"
+          justify="center"
+        >
+          <v-col cols="4"></v-col>
+          <v-col cols="5" class="d-flex justify-start">
+            <h3>Coordenadores</h3>
+          </v-col>
+          <v-col cols="3"></v-col>
+          <v-col cols="4"></v-col>
+          <v-col cols="5" class="d-flex justify-start">
+            <p style="text-align: left;">
+              <span>Marcus Vinicius Ribeiro de Carvalho - mvcarvalhopi@ctu.uespi.br</span>
+              <br />
+              <span>Ellem Francine Barbosa - francine@usp.br</span>
+            </p>
+          </v-col>
+          <v-col cols="3"></v-col>
+
+          <v-col cols="4"></v-col>
+          <v-col cols="5" class="d-flex justify-start">
+            <h3>Desenvolvedores</h3>
+          </v-col>
+          <v-col cols="3"></v-col>
+          <v-col cols="4"></v-col>
+          <v-col cols="5" class="d-flex justify-start">
+            <p style="text-align: left;">
+              <span>Matheus William Gomes dos Santos - matheuswg99@gmail.com</span>
+              <br />
+              <span>Vitor Azevedo</span>
+            </p>
+          </v-col>
+          <v-col cols="3"></v-col>
+          <v-col cols="4"></v-col>
+          <v-col cols="5" class="d-flex justify-start">
+            <v-card>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                width="180px"
+              ></v-img>
+
+              <v-card-actions>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <span>
+                    <strong>Nome1</strong>
+                  </span>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+            <v-card class="ml-1">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                width="180px"
+              ></v-img>
+
+              <v-card-actions>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <span>
+                    <strong>Nome2</strong>
+                  </span>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+            <v-card class="ml-1">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                width="180px"
+              ></v-img>
+
+              <v-card-actions>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <span>
+                    <strong>Nome3</strong>
+                  </span>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="3"></v-col>
+        </v-row>
+      </v-tab-item>
+    </v-tabs-items>
   </v-row>
 </template>
 
@@ -50,10 +168,28 @@ export default {
       messageClass: "",
       messageTitle: "",
       message: "",
-      buttons: ["contato", "publicações", "projetos"]
+      atual_page: "",
+      pages: [
+        {
+          title: "Educom Tool!",
+          subtitle: "Educational content modelling",
+          img_src: require("@/assets/home/bg1.jpg")
+        },
+        {
+          title: "Projeto",
+          subtitle: "",
+          img_src: require("@/assets/home/bg2.jpg")
+        },
+        {
+          title: "Equipe",
+          subtitle: "",
+          img_src: require("@/assets/home/bg3.jpg")
+        }
+      ]
     };
   },
   mounted() {
+    this.atual_page = this.pages[0];
     if (this.$route.params.emailconfirmation === "1") {
       this.messageClass = "headline green";
       this.messageTitle = "Email confirmado com sucesso";
