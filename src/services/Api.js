ea3ffd0f286e777c58b24ee488bbb9c9bb28ded9
Matheus William <matheuswg99@gmail.com>
2020-08-11 20:24:09 -0300

    Resolvendo bug abertura mobilemedias

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: `https://educomml-back.herokuapp.com/`,
+    baseURL: "https://educomml-back.herokuapp.com/",
     withCredentials: false,
     headers: {
       Accept: "application/json",
